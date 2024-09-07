8019f3e865d98fb2894b078cb07c8bcf9a6b75ec
rbom482 <163361794+rbom482@users.noreply.github.com>
2024-09-06 22:13:27 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,34 +6,34 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
-    # Path for registration
+ # Path for registration
     path('register', views.registration, name='register'),
 
-    # Path for login/logout
+ # Path for login/logout
     path('login', views.login_user, name='login'),
     path('logout', views.logout_request, name='logout'),
 
-    # Path for getting cars
+ # Path for getting cars
     path('get_cars', views.get_cars, name='getcars'),
 
-# Path for dealer reviews and details
-path('get_dealers/', views.get_dealerships, name='get_dealers'),
-path(
-    'get_dealers/<str:state>', 
-    views.get_dealerships, 
-    name='get_dealers_by_state'
-),
-path(
-    'dealer/<int:dealer_id>', 
-    views.get_dealer_details, 
-    name='dealer_details'
-),
-path(
-    'reviews/dealer/<int:dealer_id>', 
-    views.get_dealer_reviews, 
-    name='get_dealer_reviews'
-),
+ # Path for dealer reviews and details
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),
+    path(
+        'get_dealers/<str:state>',
+        views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        'dealer/<int:dealer_id>',
+        views.get_dealer_details,
+        name='dealer_details'
+    ),
+    path(
+        'reviews/dealer/<int:dealer_id>',
+        views.get_dealer_reviews,
+        name='get_dealer_reviews'
+    ),
 
-# Path for adding a review
-path('add_review', views.add_review, name='add_review'),
+ # Path for adding a review
+    path('add_review', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
